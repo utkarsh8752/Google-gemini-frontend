c0c41e4f690e065d38086cd7a89470101b9fd1f7
Utkarsh mishra <eng.utkarshmishra@gmail.com>
2026-03-05 12:11:22 +0530

    changes url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,4 @@
-// import { useState } from "react";
-// import axios from "axios";
-// import "./App.css";
 
-// function App() {
-//   const [message, setMessage] = useState("");
-//   const [chat, setChat] = useState([]);
-
-//  const sendMessage = async () => {
-//   if (!message) return;
-
-//   const newChat = [...chat, { sender: "user", text: message }];
-//   setChat(newChat);
-
-
-//   try {
-//     const res = await axios.post("http://localhost:5000/ask", {
-//       message: message,
-//     });
-
-//     setChat([...newChat, { sender: "bot", text: res.data.reply }]);
-//     setMessage("");
-//   } catch (err) {
-//     console.log("Frontend error:", err);
-//   }
-// };
-//   return (
-//     <div className="app">
-//       <h2>Gemini AI Chat</h2>
-
-//       <div className="chat-box">
-//         {chat.map((c, i) => (
-//           <div key={i} className={c.sender}>
-//             {c.sender === "user" ? "You: " : "Gemini: "}
-//             {c.text}
-//           </div>
-//         ))}
-//       </div>
-
-//       <div className="input-area">
-//         <input
-//           value={message}
-//           onChange={(e) => setMessage(e.target.value)}
-//           placeholder="Ask something..."
-//         />
-//         <button onClick={sendMessage}>Send</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 import { useState } from "react";
@@ -69,7 +18,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:3000/api/content", {
+      const res = await axios.post("https://google-gemini-backend.onrender.com/api/content", {
         question: message, // must match backend
       });
 
